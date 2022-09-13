2f52876d20ddab1d370bac26c47d0cede1c6632a
PoojaVeera <110709890+PoojaVeera@users.noreply.github.com>
2022-09-13 12:32:06 +0530

    modified:   src/components/TablePage.js

--- src/components/TablePage.js
+++ src/components/TablePage.js
@@ -6,6 +6,7 @@ import {
   TableCell,
   TableRow,
   Paper,
+  Button,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import React from "react";
@@ -24,43 +25,35 @@ export const TablePage = () => {
     <div>
       TablePage
       <TableContainer component={Paper} align="center">
-        <Table size="small" sx={{ minWidth: 650 }}>
-          <TableHead>
+        <Table size="small" sx={{ minWidth: 650, backgroundColor: "pink" }}>
+          <TableHead backgroundColor="pink">
             <TableCell align="center"> FIRST NAME</TableCell>
             <TableCell align="center"> LAST NAME</TableCell>
             <TableCell align="center"> ID </TableCell>
             <TableCell align="center"> MOBILE</TableCell>
             <TableCell align="center"> EMAIL</TableCell>
-            <TableCell align="center"> del</TableCell>
-            <TableCell align="center"> Edit</TableCell>
+            <Button
+              variant="contained"
+              size="small"
+              onClick={() => alert("EDITED")}
+            >
+              Edited
+            </Button>
+            <TableCell align="center"> </TableCell>
+            <TableCell align="center">
+              <Button onClick={() => alert("deleted")}>Delete</Button>
+            </TableCell>
           </TableHead>
           <TableBody align="left">
             <StyledTableRow>
-              <TableCell> </TableCell>
-              <TableCell> FIRST NAME</TableCell>
-              <TableCell> FIRST NAME</TableCell>
-              <TableCell> FIRST NAME</TableCell>
-              <TableCell> FIRST NAME</TableCell>
+              <TableCell align="center"> Edit</TableCell>
+              <TableCell align="center"> Edit</TableCell>
+              <TableCell align="center"> Edit</TableCell>
             </StyledTableRow>
             <StyledTableRow>
-              <TableCell> FIRST NAME</TableCell>
-              <TableCell>fn </TableCell>
-              <TableCell> FIRST NAME</TableCell>
-              <TableCell> FIRST NAME</TableCell>
-              <TableCell> FIRST NAME</TableCell>
-            </StyledTableRow>
-            <StyledTableRow>
-              <TableCell> FIRST NAME</TableCell>
-              <TableCell> fn</TableCell>
-              <TableCell> FIRST NAME</TableCell>
-              <TableCell> FIRST NAME</TableCell>
-              <TableCell> FIRST NAME</TableCell>
-            </StyledTableRow>
-            <StyledTableRow>
-              <TableCell> </TableCell>
-              <TableCell> FIRST NAME</TableCell>
-              <TableCell> FIRST NAME</TableCell>
-              <TableCell> FIRST NAME</TableCell>
+              <TableCell align="center"> Edit</TableCell>
+              <TableCell align="center"> Edit</TableCell>
+              <TableCell align="center"> Edit</TableCell>
             </StyledTableRow>
           </TableBody>
         </Table>
